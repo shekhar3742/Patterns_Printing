1dfa8ec668a75ba326a6badd033241c168f3a921
Shekhar3742 <imderek3742@gmail.com>
2025-05-26 23:43:26 +0530

    next

--- patterns.cpp
+++ patterns.cpp
@@ -85,6 +85,68 @@ void pattern8(int N){
 	}
 }
 
+void pattern9(int N){
+	for(int i=1 ; i<=N; i++){
+		for(int j=1; j<=(N-i); j++){
+			cout<< " ";
+		}
+		for(int j=1; j<=(2*i-1); j++){
+			cout<<"*";
+		}
+		for(int j=1; j<=(N-i); j++){
+			cout<< " ";
+		}
+		cout<<endl;
+		
+	}
+	for(int i=1; i<=N;i++){
+		for(int j=1 ; j<i; j++){
+			cout<<" ";
+		}
+		for(int j=1; j<=2*N-(2*i-1); j++){
+			cout<<"*";
+		}
+		for(int j=1 ; j<i; j++){
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+void pattern10(int N){
+	for(int i=1; i<=N;i++){
+		for(int j=1; j<=i; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	for(int i=1; i<=(N-1); i++){
+		for(int j=1; j<=(N-i); j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+void pattern11(int N){
+	for(int i=1; i<=N; i++){
+		int start = 1;
+		if(i % 2 == 0) start= 0 ;
+		else start = 1;
+		for(int j=1 ; j<=i; j++){
+			cout<<start;
+			start = 1-start;
+		}
+		cout<<endl;
+	}
+}
+
+void pattern12(int N){
+	
+}
+
+
+
 
 
 
@@ -99,6 +161,9 @@ int main(){
 //  pattern5(N);
 //	pattern6(N);
 //	pattern7(N);
-	pattern8(N);
+//	pattern8(N);
+//	pattern9(N);
+//	pattern10(N);
+	pattern11(N);
 	return 0 ;
 }
